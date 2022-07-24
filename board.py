23234f8a05b84effa648ff266d40014582453639
Minh Vy Ha <vyha0512@gmail.com>
2022-07-24 14:40:44 +1000

    update

--- board.py
+++ board.py
@@ -107,6 +107,6 @@ class Block:
         self.value = 0
 
     def draw(self):
-        if self.value != -1:
+        if self.value != 0:
             num = FONT.render(f'{self.value}', 1, BLACK)
             self.WIN.blit(num, (self.width // 9 * self.row + (self.width // 9 // 2) - num.get_width() // 2, self.padding + (self.height // 9) * self.col + self.height // 9 // 2 - num.get_height() // 2))
